29b5a8859520c9759fbc082d72c303077d85b44d
Samsung Campus 24 <aramasatryan290@gmail.com>
2024-03-02 16:33:32 +0400

    New message Hello World

--- app/src/main/java/com/example/a24club/MainActivity.java
+++ app/src/main/java/com/example/a24club/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ystem.out.println("Hello World")
+        System.out.println(1);
     }
 }
