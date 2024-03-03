5fea7672040d508f4c5f023dac75e5b5f43e6bac
arama <aramasatryan290@gmail.com>
2024-03-03 08:15:31 -0800

    Quiz

---
+++ app/src/main/java/com/example/a24club/QuestionAnswer.java
@@ -0,0 +1,15 @@
+package com.example.a24club;
+
+public class QuestionAnswer {
+    public static String question[] = {
+        "Which team didn't make it to the semi-finals in 2008", "UCL 2023 Winner"
+    };
+    public static String choices[][] = {
+            {"Man United", "Real Madrid", "Barcelona", "Liverpool"},
+            {"Man City", "Eindhoven", "Arsenal", "PSG"}
+    };
+     public static String correctAnswers[] = {
+        "Real Madrid",
+             "Man City"
+    };
+}
