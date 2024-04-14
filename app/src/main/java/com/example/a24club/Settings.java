37350821ac1a8cd8672aeea8e310a3bb1d938df6
arama <aramasatryan290@gmail.com>
2024-04-14 08:01:32 -0700

    Logout Background Color

--- app/src/main/java/com/example/a24club/Settings.java
+++ app/src/main/java/com/example/a24club/Settings.java
@@ -1,20 +1,40 @@
 package com.example.a24club;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Settings extends AppCompatActivity {
-    Button button;
+    Button buttonc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        button = findViewById(R.id.logout_btn);
+
+        buttonc = findViewById(R.id.logout_btn);
+        buttonc.setBackgroundTintList(ColorStateList.valueOf(Color.BLACK));
+        buttonc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Clear isLoggedIn in SharedPreferences
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", false);
+                editor.apply();
+
+                // Navigate to LoginActivity (or any other login screen)
+                Intent intent = new Intent(Settings.this, Login.class);
+                startActivity(intent);
+                finish(); // Finish the Settings activity
+            }
+        });
     }
 }
