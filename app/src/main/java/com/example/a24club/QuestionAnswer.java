9d55f329292ffc379e1e3ff81d213bb9dfa33626
arama <aramasatryan290@gmail.com>
2024-05-12 09:47:31 -0700

    More questions added

--- app/src/main/java/com/example/a24club/QuestionAnswer.java
+++ app/src/main/java/com/example/a24club/QuestionAnswer.java
@@ -2,14 +2,42 @@ package com.example.a24club;
 
 public class QuestionAnswer {
     public static String[] question = {
-        "Which team didn't make it to the semi-finals in 2008", "UCL 2023 Winner"
+            "Which team didn't make it to the semi-finals in 2008?",
+            "UCL 2023 Winner?",
+            "Who holds the record for the most goals in a single UEFA Champions League season?",
+            "Which club has won the most UEFA Champions League titles?",
+            "Which country's teams have won the most cumulative Champions League titles?",
+            "Which player scored the fastest goal in a UEFA Champions League final?",
+            "What was the original name of the UEFA Champions League before 1992?",
+            "Which team achieved the highest score in a single Champions League match?",
+            "Who is the youngest player to have scored in a UEFA Champions League final?",
+            "Which city hosted the 1999 UEFA Champions League final?"
     };
+
     public static String[][] choices = {
             {"Man United", "Real Madrid", "Barcelona", "Liverpool"},
-            {"Man City", "Eindhoven", "Arsenal", "PSG"}
+            {"Man City", "Eindhoven", "Arsenal", "PSG"},
+            {"Lionel Messi", "Cristiano Ronaldo", "Robert Lewandowski", "Ruud van Nistelrooy"},
+            {"Real Madrid", "AC Milan", "Liverpool", "Bayern Munich"},
+            {"Spain", "England", "Italy", "Germany"},
+            {"Paolo Maldini", "Cristiano Ronaldo", "Gareth Bale", "Roy Makaay"},
+            {"European Champion Clubs' Cup", "European Premier League", "European Football League", "Euro Cup"},
+            {"Paris Saint-Germain", "Real Madrid", "Liverpool", "Barcelona"},
+            {"Patrick Kluivert", "Kylian Mbappe", "Marcus Rashford", "Lionel Messi"},
+            {"London", "Paris", "Barcelona", "Munich"}
     };
-     public static String[] correctAnswers = {
-        "Real Madrid",
-             "Man City"
+
+    public static String[] correctAnswers = {
+            "Real Madrid",
+            "Man City",
+            "Cristiano Ronaldo",
+            "Real Madrid",
+            "Spain",
+            "Paolo Maldini",
+            "European Champion Clubs' Cup",
+            "Paris Saint-Germain",
+            "Patrick Kluivert",
+            "Barcelona"
     };
 }
+
